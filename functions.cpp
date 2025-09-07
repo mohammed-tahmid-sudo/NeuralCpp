885a7749af25014d51bc5c03e9c283e87916b2f8
mohammed-tahmid-sudo <tahmid@DESKTOP-IQLI97J.localdomain>
2025-09-07 16:14:59 +0600

    Coded a Single Neuron. Now a 1000 or something like that left

---
+++ functions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <random>
+#include <vector>
+
+class Neuron {
+public:
+  float neuron(const std::vector<double>& input) {
+    float sum = 0.0f;
+
+    std::vector<double> output;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> uniform_dist(-0.01, 0.01);
+
+    for (int i = 0; i < input.size(); i++) {
+      double weight = uniform_dist(gen);
+      double bias = uniform_dist(gen);
+       sum += input[i] * weight + bias;
+
+    }
+    return sum;
+  }
+};
+
+int main() {
+    Neuron n;
+    std::vector<double> input = {1.0, 2.0, 3.0};
+    float output = n.neuron(input);                    //////////////////// changed output type from vector<double> to float
+
+    std::cout << "Neuron output: " << output << std::endl;
+    return 0;
+}
