2a8e4fb10feabdc37f24c614d2565ff26996c257
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-09-08 14:04:53 +0600

    update

--- functions.cpp
+++ functions.cpp
@@ -2,28 +2,46 @@
 #include <random>
 #include <vector>
 
+std::random_device rd;  // Non-deterministic seed
+std::mt19937 gen(rd()); // Mersenne Twister RNG
+std::uniform_real_distribution<> dist(-0.5, 0.5); // Random float between -0.5 and 0.5
+                                                  
+
+std::vector<double> weights;
+std::vector<double> bias;
 
 
 
-float neuron(const std::vector<double>& input, const std::vector<double>& weights, const double biass) {
+double neuron(const std::vector<double>& input, const std::vector<double>& weights, const double biass) {
              
-    float sum = 0.0f;
-
-    std::vector<double> output;
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_real_distribution<> uniform_dist(-0.01, 0.01);
-
+    double sum = 0.0f;
     for (int i = 0; i < input.size(); i++) {
-      // double weight = uniform_dist(gen);
-      // double bias = uniform_dist(gen);
        sum += input[i] * weights[i];
 
     }
     return sum += biass;
   }
 
-float ReLU(float x) {
+double LLayer(int in_features, int out_features, std::vector<double> weights, std::vector<double> bias) {
+        weights(in_features);
+        bias(out_features);
+         for (int i = 0; i < in_features; i++) {
+                weights[i] = dist(gen);  // Generate random number
+         }
+
+         for (int i = 0; i <out_features; i++) {
+                bias[i] = dist(gen);  // Generate random number
+         }
+
+         for (int i = 0; i < weights.size(); i++) {
+                 return weights[i];    // Generate random number
+         }
+
+
+
+}
+
+double ReLU(double x) {
         if (x > 0) {
                 return x;
         } else {
@@ -32,10 +50,12 @@ float ReLU(float x) {
 }
 
 int main() {
-    std::vector<double> input = {1.0, 2.0, 3.0};
-    std::vector<double> weights = {3.0, -345.0, -33.0};
-    double bias = -5.333333;
-    float output = ReLU(neuron(input, weights, bias  ));            
-    std::cout << "Neuron output: " << output << std::endl;
+    // std::vector<double> input = {1.0, 2.0, 3.0};
+    // std::vector<double> weights = {3.0, 345.0, -33.0};
+    // double bias = 5.333333;
+    // double output = ReLU(neuron(input, weights, bias  ));            
+    std::cout << LLayer(5, 5, weights, bias) << std::endl;
+
+    // std::cout << "Neuron output: " << output << std::endl;
     return 0;
 }
